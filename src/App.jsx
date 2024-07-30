fc893a50e57eeee5a70e30e5680f40d8e330477d
Homer Darang <119770824+homerdarang@users.noreply.github.com>
2024-07-31 02:15:38 +0800

    starting ReactJS with CSS-BEM Methodology

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div className='title'>
+        <p className=''>ReactJS | BEM_CSS</p>
+      </div>
+    </>
+  )
+}
+
+export default App

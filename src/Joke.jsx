88a64312fe8420434fe603192773baf55989ff26
Homer Darang <119770824+homerdarang@users.noreply.github.com>
2024-08-05 01:06:41 +0800

    refactored codes in Joke Components and cardDisplay Component

--- src/Joke.jsx
+++ src/Joke.jsx
@@ -3,9 +3,6 @@ import React from 'react'
 const Joke = (props) => {
   return (
     <div className='joke'>
-      {/* {props.setup && <h3 className='font-medium text-2xl'>Setup: {props.setup}</h3>}
-      {props.punchline && <p className='text-xl font-extralight'>Punchline: {props.punchline}</p>} */}
-
       {props.isPun && props.setup && <h3 className='font-medium text-3xl'>setup:{props.setup}, upvotes: {props.upvotes}</h3>}
       {props.isPun && props.punchline && <p className='text-xl font-extralight'>punchline: {props.punchline}, downvotes: {props.downvotes}</p>}
       <hr />
--- src/components/card/CardDisplay.jsx
+++ src/components/card/CardDisplay.jsx
@@ -7,7 +7,7 @@ const CardDisplay = (props) => {
             {props.image && <img className='cards--img' src={props.image} alt="" />}
             <div className='card--info'>
                 {props.rating && <p>Rating: {props.rating}</p>}
-                {props.reviewCounts && <p>Reviews: {props.reviewCounts}</p>}
+                {props.reviewCounts && <p>Reviews: ({props.reviewCounts})</p>}
                 {props.country && <p>{props.country}</p>}
             </div>
             {props.title && <p>{props.title}</p>}

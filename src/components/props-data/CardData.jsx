debd53f3aac60239e3bed6c9b837776028a94f95
Homer Darang <119770824+homerdarang@users.noreply.github.com>
2024-08-05 01:23:18 +0800

    added details in CardData component for visitors/users

--- src/components/props-data/CardData.jsx
+++ src/components/props-data/CardData.jsx
@@ -5,7 +5,7 @@ export default [
     {
         id: 1,
         image: airBnbimg,
-        rating: '5.0',
+        rating: '4.3',
         reviewCounts: 6,
         country: 'Philippines',
         title: 'Life lesson with spyda answer',
@@ -15,18 +15,18 @@ export default [
         id: 2,
         image: kitchen,
         rating: '5.0',
-        reviewCounts: 6,
+        reviewCounts: 17,
         country: 'Philippines',
-        title: 'Life lesson with spyda answer',
-        price: 'Php2,000.00'
+        title: 'Best of small and affordable room for week vacation',
+        price: 'Php3,200.00'
     },
     {
         id: 3,
         image: room,
-        rating: '5.0',
+        rating: '4.8.0',
         reviewCounts: 6,
         country: 'Philippines',
-        title: 'Life lesson with spyda answer',
-        price: 'Php2,000.00'
+        title: 'A native and modern kitchen that everybody is looking for.',
+        price: 'Php4,500.00'
     }
 ]

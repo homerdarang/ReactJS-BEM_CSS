3bd76a638d6720bbc8c8fbbf6aea3c83b35bec25
Homer Darang <119770824+homerdarang@users.noreply.github.com>
2024-08-05 01:14:53 +0800

    refactored punchline prop in Joke component

--- src/Joke.jsx
+++ src/Joke.jsx
@@ -4,7 +4,7 @@ const Joke = (props) => {
   return (
     <div className='joke'>
       {props.isPun && props.setup && <h3 className='font-medium text-3xl'>setup:{props.setup}, upvotes: {props.upvotes}</h3>}
-      {props.isPun && props.punchline && <p className='text-xl font-extralight'>punchline: {props.punchline}, downvotes: {props.downvotes}</p>}
+      {props.punchline && <p className='text-xl font-extralight'>punchline: {props.punchline}, downvotes: {props.downvotes}</p>}
       <hr />
     </div>
   )
